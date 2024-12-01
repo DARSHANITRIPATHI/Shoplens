ad381ea05a3aab5e453879e9a9b441d25a02471d
Darshani Tripathi <116213630+DARSHANITRIPATHI@users.noreply.github.com>
2024-12-01 12:58:40 +0530

    Update backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,16 +22,4 @@ export const getProductById = async (req, res) => {
   }
 };
 
-// Controller to fetch a single product by ID
-// export const getProductById = async (req, res) => {
-//   try {
-//     const product = await Product.findOne({ productId: req.params.id });
-//     if (product) {
-//       res.json(product);
-//     } else {
-//       res.status(404).json({ message: "Product not found" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: "Error fetching product" });
-//   }
-// };
+
